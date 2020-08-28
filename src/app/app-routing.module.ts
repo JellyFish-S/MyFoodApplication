0d0898e770f6c300a14713f3cfff5f2ffaf16028
Svetlana <jellyfdesign@gmail.com>
2020-08-28 18:57:40 +0300

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MainpageLogoutComponent} from './mainpage-logout/mainpage-logout.component';
+import {MainpageLoginComponent} from './mainpage-login/mainpage-login.component';
+import {AccountCreationComponent} from './account-creation/account-creation.component';
+import {FoodDiaryComponent} from './food-diary/food-diary.component';
+import {StatisticsComponent} from './statistics/statistics.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainpageLogoutComponent},
+  {path: 'login', component: MainpageLoginComponent},
+  {path: 'registration', component: AccountCreationComponent},
+  {path: 'diary', component: FoodDiaryComponent},
+  {path: 'statistics', component: StatisticsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/food-diary/food-diary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mf-food-diary',
+  templateUrl: './food-diary.component.html',
+  styleUrls: ['./food-diary.component.scss']
+})
+export class FoodDiaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
