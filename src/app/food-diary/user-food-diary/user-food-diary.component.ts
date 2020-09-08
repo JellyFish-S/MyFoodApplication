545c3aa9edbc6b037927aa1c6c17e37ba1aa388a
Svetlana <jellyfdesign@gmail.com>
2020-09-08 19:36:12 +0300

    Add interface for product information, add to firebase some products

--- src/app/food-diary/user-food-diary/user-food-diary.component.ts
+++ src/app/food-diary/user-food-diary/user-food-diary.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {DateService} from '../../services/date.service';
 
+
+
 @Component({
   selector: 'mf-user-food-diary',
   templateUrl: './user-food-diary.component.html',
@@ -8,10 +10,25 @@ import {DateService} from '../../services/date.service';
 })
 export class UserFoodDiaryComponent implements OnInit {
 
-  constructor(public  dateService: DateService) { }
+  constructor(
+    public  dateService: DateService
+    ) { }
 
   ngOnInit(): void {
   }
 
+  // sendProductToFB() {
+  //   const product: ProductsDB = {
+  //     name: 'Coffee Cappuccino',
+  //     protein: 2,
+  //     fat: 2.4,
+  //     carbohydrate: 3,
+  //     calories: 42,
+  //     weight: 100
+  //   };
+  //   this.createFoodDatabaseService.create(product).subscribe(() => {
+  //     console.log('success');
+  //   });
+  // }
 
 }
