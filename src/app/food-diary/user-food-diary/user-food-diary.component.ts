1a70e0388bd7fcbab00f7ba13465d3b2c2c84665
Svetlana <jellyfdesign@gmail.com>
2020-09-08 16:36:41 +0300

    Chain calendar selected date with user-food-diary.component

--- src/app/food-diary/user-food-diary/user-food-diary.component.ts
+++ src/app/food-diary/user-food-diary/user-food-diary.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {DateService} from '../../services/date.service';
 
 @Component({
   selector: 'mf-user-food-diary',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserFoodDiaryComponent implements OnInit {
 
-  constructor() { }
+  constructor(public  dateService: DateService) { }
 
   ngOnInit(): void {
   }
