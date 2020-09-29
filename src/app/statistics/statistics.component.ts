72315836f3d23d61b29b9f5e22fd672e23c74f48
Svetlana <jellyfdesign@gmail.com>
2020-09-29 13:41:20 +0300

    Add graphic by months, which user choose, to see calories per day and calorie goal

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -17,7 +17,15 @@ export class StatisticsComponent implements OnInit {
   private goalWeightArr = [];
   private dateArr = [];
   optionWeight: any;
+  optionCalories: any;
+  month: string;
   private subscriptionWeight: Subscription;
+  private sumCalories: number;
+  private subscription: Subscription;
+  private calorieMonth = [];
+  private dateMonth = [];
+  private caloriesMonth = [];
+  private goalCalorieArr = [];
 
   constructor(
     private checkUserIdService: CheckUserIdService,
@@ -35,8 +43,15 @@ export class StatisticsComponent implements OnInit {
         this.createWeightProgressGraphic();
       });
     });
-  }
 
+    this.sumCalories = this.userInformation.caloriesGoal;
+    console.log(this.sumCalories);
+    this.subscription = this.accountService.subject.subscribe(message => {
+      this.sumCalories = message;
+      console.log(this.sumCalories);
+  });
+
+  }
   async GetUserWeight(): Promise<UserWeight[]> {
     this.weightArr = [];
     this.goalWeightArr = [];
@@ -98,6 +113,116 @@ export class StatisticsComponent implements OnInit {
           name: 'Current weight',
           type: 'line',
           data: this.weightArr
+        },
+
+      ],
+      color: [
+        '#F37EA1', '#5E14BC'
+      ]
+    };
+  }
+
+  async GetFoodFromFirebase(food: string, arrCalories): Promise<any> {
+    const dataSnapShot = await firebase.database().ref(`${food}/`).once('value');
+    const objDatabase = dataSnapShot.val();
+    const user = await firebase.auth().currentUser;
+    // tslint:disable-next-line:forin
+    for (const key in objDatabase) {
+      for (const i in objDatabase[key]) {
+        if (objDatabase[key][i].userId === user.uid) {
+          const calString = objDatabase[key][i].calories;
+          const dateString = objDatabase[key][i].date;
+          const monthString = objDatabase[key][i].date.substring(3, 5);
+          arrCalories.push({date: dateString, calories: calString, month: monthString});
+        }
+      }
+    }
+    this.sumArray(arrCalories);
+    return arrCalories;
+  }
+
+  sumArray(calories): void {
+    let q = 1;
+    while (q < calories.length) {
+      if (calories[q].date === calories[q - 1].date) {
+        calories[q - 1].calories = calories[q - 1].calories + calories[q].calories;
+        calories.splice(q, 1);
+      } else {
+        q++;
+      }
+    }
+  }
+
+  async GetAllFood(month: string): Promise<any> {
+    const breakfast = [];
+    const lunch = [];
+    const dinner = [];
+    const snack = [];
+    this.dateMonth = [];
+    this.caloriesMonth = [];
+    this.calorieMonth = [];
+    const breakfastArr = await this.GetFoodFromFirebase('breakfast', breakfast);
+    const lunchArr = await this.GetFoodFromFirebase('lunch', lunch);
+    const dinnerArr = await this.GetFoodFromFirebase('dinner', dinner);
+    const snackArr = await this.GetFoodFromFirebase('snack', snack);
+    const cal = [];
+    cal.push(...breakfastArr, ...lunchArr, ...dinnerArr, ...snackArr);
+    cal.sort((a, b) => a.date > b.date ? 1 : -1);
+    this.sumArray(cal);
+    this.calorieMonth = cal.filter(el => el.month === month);
+    this.calorieMonth.forEach(el => {
+      this.dateMonth.push(el.date.substring(0, 2));
+      this.caloriesMonth.push(el.calories);
+      this.goalCalorieArr.push(this.userInformation.caloriesGoal);
+    });
+    this.createAverageCalories();
+    console.log( this.caloriesMonth, this.dateMonth);
+    return this.calorieMonth;
+  }
+  createAverageCalories(): void {
+    this.optionCalories = {
+      title: {
+        text: 'Calories Per Day',
+        left: 'top',
+        top: 'top',
+        textStyle: {
+          fontSize: 20,
+          fontWeight: 600,
+          color: '#FF6584'
+        },
+      },
+      tooltip: {
+        trigger: 'axis'
+      },
+      legend: {
+        data: ['Goal calories', 'Today Calories']
+      },
+      grid: {
+        left: '5%',
+        right: '5%',
+        bottom: '5%',
+        containLabel: true
+      },
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: this.dateMonth
+      },
+      yAxis: {
+        type: 'value',
+        interval: 500,
+        boundaryGap: ['10%', '10%']
+      },
+      series: [
+        {
+          name: 'Goal calories',
+          type: 'line',
+          data: this.goalCalorieArr
+        },
+        {
+          name: 'Today Calories',
+          type: 'line',
+          data: this.caloriesMonth
         },
 
       ],
